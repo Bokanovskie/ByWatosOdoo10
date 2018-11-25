07b38d9b497d10887e9c6cafc7745bdeb88ff7e3
Bokanovski <alexandre.matis@hotmail.fr>
2018-11-25 15:20:03 +0100

    Création du JS qui display le burger menu, compatible avec tous les navigrateurs.

---
+++ bywatos_website_theme/static/src/js/by_watos_homepage.js
@@ -0,0 +1,50 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    var element = document.getElementById('header-bottom');
+    var asideElement = document.getElementById('sidebar-homepage');
+    var asideToggle = document.getElementById('sidebar-trigger');
+
+    if(checkVisibility(element)){
+            if(asideToggle){
+                asideToggle.style.display = 'none';
+            }
+        }else {
+            if(asideToggle){
+                asideToggle.style.display = 'block';
+            }
+        }
+
+    var mousewheelevt=(/Firefox/i.test(navigator.userAgent))? "DOMMouseScroll" : "mousewheel"
+
+    if (document.attachEvent)
+        document.attachEvent("on"+mousewheelevt, function(e){alert('Mouse wheel movement detected!')})
+    else if (document.addEventListener)
+        document.addEventListener(mousewheelevt, function(e){
+
+        if(checkVisibility(element)){
+            if(asideToggle){
+                asideToggle.style.display = 'none';
+            }
+        }else {
+            if(asideToggle){
+                asideToggle.style.display = 'block';
+            }
+        }
+
+    }, false)
+});
+
+function checkVisibility(element, evaluation){
+
+    var evaluation = evaluation = evaluation || "visible";
+
+    var viewPortHeight = window.innerHeight;
+    var scrollBarPosition = window.pageYOffset | document.body.scrollTop;
+
+    var y = element.offsetTop;
+
+    var elementHeight = element.offsetHeight;
+
+    if (evaluation == "visible") return ((y < (viewPortHeight + scrollBarPosition)) && (y > (scrollBarPosition - elementHeight)));
+    if (evaluation == "above") return ((y < (viewPortHeight + scrollBarPosition)));
+}
