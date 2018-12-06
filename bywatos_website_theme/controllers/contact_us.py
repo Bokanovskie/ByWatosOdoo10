29e9f9349104f67bd390030e771c4a7b661b70fe
Alexandre <alexandre.matis@hotmail.fr>
2018-12-06 16:18:29 +0100

    Finalisation de la page contact Creation du controller d'envoie d email

--- bywatos_website_theme/controllers/contact_us.py
+++ bywatos_website_theme/controllers/contact_us.py
@@ -5,12 +5,27 @@ from odoo.http import request
 
 
 class ContactUs(http.Controller):
+
     @http.route('/contact-us', type='http', auth='public', website=True)
     def contact_us(self):
-        values = {
 
-        }
-
-        response = request.render('bywatos_website_theme.contact_us_template', values)
+        response = request.render('bywatos_website_theme.contact_us_template')
 
         return response
+
+    @http.route('/send-contact', type='http', auth='public', website=True)
+    def send_contact(self, **post):
+
+        print(request.params)
+        mail_obj = request.env['mail.mail']
+
+        subject_str = "Prise de contact par {0} {1}".format(request.params['civility'], request.params['name'])
+
+        mail_values = {
+            'subject': subject_str,
+            'email_from': request.params['email'],
+            'body_html': request.params['message'],
+        }
+
+        mail_to_send = mail_obj.sudo().create(mail_values)
+        mail_to_send.send()
