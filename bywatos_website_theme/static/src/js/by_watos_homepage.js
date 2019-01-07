6169a20627c51f918fe9ce32c399e78d894ebb54
Bokanovski <alexandre.matis@hotmail.fr>
2019-01-07 20:17:10 +0100

    Fix bug load Js custom

--- bywatos_website_theme/static/src/js/by_watos_homepage.js
+++ bywatos_website_theme/static/src/js/by_watos_homepage.js
@@ -14,53 +14,55 @@ document.addEventListener('DOMContentLoaded', function () {
     x=w.innerWidth||e.clientWidth||g.clientWidth,
     y=w.innerHeight||e.clientHeight||g.clientHeight;
 
-    if(sidebar && asideElement.length > 0){
-        sidebar.style.background = 'transparent';
-
-        sidebar.addEventListener('mouseover', function(event){
-            sidebar.style.background = '#333';
-        });
-
-        sidebar.addEventListener('mouseout', function(event){
+    if(element){
+        if(sidebar && asideElement.length > 0){
             sidebar.style.background = 'transparent';
-        });
 
-    }
+            sidebar.addEventListener('mouseover', function(event){
+                sidebar.style.background = '#333';
+            });
 
-    if(x > 768 && asideElement.length > 0 && !contactUsElement){
-        sidebar.style.display = 'none';
-    }
+            sidebar.addEventListener('mouseout', function(event){
+                sidebar.style.background = 'transparent';
+            });
 
-    if(checkVisibility(element)){
-            if(asideToggle){
-                asideToggle.style.display = 'none';
-            }
-        }else {
-            if(asideToggle){
-                asideToggle.style.display = 'block';
-            }
         }
 
-    var mousewheelevt=(/Firefox/i.test(navigator.userAgent))? "DOMMouseScroll" : "mousewheel"
-
-    if (document.attachEvent)
-        document.attachEvent("on"+mousewheelevt, function(e){})
-    else if (document.addEventListener)
-        document.addEventListener(mousewheelevt, function(e){
+        if(x > 768 && asideElement.length > 0 && !contactUsElement){
+            sidebar.style.display = 'none';
+        }
 
         if(checkVisibility(element)){
             if(asideToggle){
                 asideToggle.style.display = 'none';
-                sidebar.style.display = 'none';
             }
         }else {
             if(asideToggle){
                 asideToggle.style.display = 'block';
-                sidebar.style.display = 'block';
             }
         }
 
-    }, false)
+        var mousewheelevt=(/Firefox/i.test(navigator.userAgent))? "DOMMouseScroll" : "mousewheel"
+
+        if (document.attachEvent)
+            document.attachEvent("on"+mousewheelevt, function(e){})
+        else if (document.addEventListener)
+            document.addEventListener(mousewheelevt, function(e){
+
+                if(checkVisibility(element)){
+                    if(asideToggle){
+                        asideToggle.style.display = 'none';
+                        sidebar.style.display = 'none';
+                    }
+                }else {
+                    if(asideToggle){
+                        asideToggle.style.display = 'block';
+                        sidebar.style.display = 'block';
+                    }
+                }
+
+            }, false)
+    }
 });
 
 function checkVisibility(element, evaluation){
