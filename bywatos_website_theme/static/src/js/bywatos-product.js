9c16947aae175683708ebf6895c09b245d041f5a
Bokanovski <alexandre.matis@hotmail.fr>
2019-02-23 17:47:22 +0100

    Gestion de l'affichage et désaffichage de la modal

--- bywatos_website_theme/static/src/js/bywatos-product.js
+++ bywatos_website_theme/static/src/js/bywatos-product.js
@@ -1,8 +1,38 @@
 odoo.define('bywatos_website_theme.main', function (require) {
     document.addEventListener('DOMContentLoaded', function () {
 
-        var modalElement= document.getElementById('descriptionProductModal');
+        // Product description modal
+        var modalElement = document.getElementById('description-product-modal');
 
+        // Button to display modal
+        var continuousButton = document.getElementById('continuous-button');
+
+        // Button to close modal
+        var closeButtonModal = document.getElementById('close-button-modal');
+
+
+        if(continuousButton && modalElement && closeButtonModal){
+
+            // Add event click listener on continuous button to open modal
+            continuousButton.addEventListener('click', () => {
+
+                modalElement.classList.remove('hidden');
+                modalElement.classList.add('show');
+                
+                positionModal(modalElement);
+
+            });
+
+            // Add event click listener on continuous button to close modal
+            closeButtonModal.addEventListener('click', () => {
+
+                modalElement.classList.remove('show');
+                modalElement.classList.add('hidden');
+
+            });
+        }
+
+        // Function execute on page resize
         function resize() {
             if(modalElement){
                 positionModal(modalElement);
@@ -10,16 +40,22 @@ odoo.define('bywatos_website_theme.main', function (require) {
 
         }
 
+        // Function to position the modal
         function positionModal(modalElement){
 
             let modalWidth = modalElement.offsetWidth;
             let windowWidth = window.innerWidth;
 
             let modalLeftPosition = ((windowWidth / 2) - (modalWidth / 2));
+
             modalElement.style.left = modalLeftPosition + 'px';
         }
 
         positionModal(modalElement);
+
+        modalElement.classList.remove('show');
+        modalElement.classList.add('hidden');
+
         window.onresize = resize;
 
     });
